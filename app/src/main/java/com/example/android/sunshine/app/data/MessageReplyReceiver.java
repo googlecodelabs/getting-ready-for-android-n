048527ce4831316876487e0e7bc78e7f28c73d4d
Paul Rashidi <paulrashidi@google.com>
2016-05-16 15:14:03 -0700

    Notifications complete checkpoint.
    Change-Id: I3fa34ed12c91d6422b7861d920314369e1cc92f3

--- app/src/main/java/com/example/android/sunshine/app/data/MessageReplyReceiver.java
+++ app/src/main/java/com/example/android/sunshine/app/data/MessageReplyReceiver.java
@@ -10,6 +10,7 @@ import android.support.v4.app.RemoteInput;
 import android.util.Log;
 
 import com.example.android.sunshine.app.BuildConfig;
+import com.example.android.sunshine.app.sync.SunshineSyncEngine;
 
 /**
  * A receiver that gets called when a reply is sent to a given conversationId
@@ -17,32 +18,36 @@ import com.example.android.sunshine.app.BuildConfig;
 public class MessageReplyReceiver extends BroadcastReceiver {
 
     public static final String EXTRA_REMOTE_REPLY = "extra_remote_reply";
-    // public static final String REPLY_ACTION = BuildConfig.APPLICATION_ID + ".ACTION_MESSAGE_REPLY";
-    // public static final String EXTRA_FORECAST = "extra_forecast";
+    public static final String REPLY_ACTION = BuildConfig.APPLICATION_ID + ".ACTION_MESSAGE_REPLY";
+    public static final String EXTRA_FORECAST = "extra_forecast";
 
     private static final String TAG = MessageReplyReceiver.class.getSimpleName();
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        /*
         if (REPLY_ACTION.equals(intent.getAction())) {
             Forecast forecast = intent.getParcelableExtra(EXTRA_FORECAST);
             CharSequence reply = getMessageText(intent);
             if (forecast != null) {
                 Log.w(TAG, "Got reply (" + reply + ") for Forecast " + forecast);
+                NotificationManager notificationManager =
+                        (NotificationManager) context.getSystemService(
+                                Context.NOTIFICATION_SERVICE);
+                NotificationCompat.Builder notificationBuilder =
+                        SunshineSyncEngine.createForecastNotificationBuilder(
+                                context, forecast);
+                notificationManager.notify(forecast.mDaysSinceEpoch,
+                        notificationBuilder.build());
             }
         }
-        */
     }
 
-    /*
     public static Intent getMessageReplyIntent(Forecast forecast) {
         return new Intent()
                 .addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES)
                 .setAction(MessageReplyReceiver.REPLY_ACTION)
                 .putExtra(MessageReplyReceiver.EXTRA_FORECAST, forecast);
     }
-    */
 
     /**
      * Get the message text from the intent.
